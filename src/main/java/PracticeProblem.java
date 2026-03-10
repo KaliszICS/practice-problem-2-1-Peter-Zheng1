0a78d8839f4059f077b4f53872c299d3ecfcf298
Peter-Zheng1 <343277125@tdsb.ca>
2026-03-10 15:00:33 +0000

    done

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -5,5 +5,30 @@ public class PracticeProblem {
 	}
 
 	//Create your functions here
+	public static int sum(int num1, int num2){
+		int result;
+		result = num1 + num2;
+		return result;
+	}
+	
+	public static int difference(int num1, int num2){
+		int result;
+		result = num1 - num2;
+		return result;
+		
+	}
 
+	public static double product(double num1, double num2){
+		double result;
+		result = num1 * num2;
+		return result;
+
+	}
+
+	public static String removeFirst(String wordy){
+		String newword;
+		newword = wordy.substring(1);
+		return newword;
+
+	}
 }
